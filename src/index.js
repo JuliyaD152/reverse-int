f3f093f5473bfe46301fb30ff9cb2605604b72ec
Yuliya Belyavskaya <JuliyaD152@yandex.by>
2020-10-01 18:59:55 +0200

    docs: add ready varient

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
-}
+    let newNum = Math.abs(n);
+    let reverseNum = newNum.toString().split('').reverse().join('');
+    return Number(reverseNum); 
+};
+
